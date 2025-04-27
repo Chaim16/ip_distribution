e036be1ca9407b22a8b3201ea35427710699c163
chaim16 <yqm.0229@qq.com>
2025-04-27 23:58:21 +0800

    feat: 键库表

--- distribution/view/serilazer.py
+++ distribution/view/serilazer.py
@@ -22,45 +22,6 @@ class UserModifySerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=64, required=True, help_text="昵称")
 
 
-class ApplyAsDesignerSerializer(serializers.Serializer):
-
-    reason = serializers.CharField(max_length=512, required=True, help_text="申请理由")
-
-
-class RechargeSerializer(serializers.Serializer):
-
-    amount = serializers.FloatField(required=True, help_text="充值金额")
-
-
-class ApproveDesignerApplicationSerializer(serializers.Serializer):
-
-    status = serializers.ChoiceField(choices=[
-        (DesignerApplicationStatus.PASS.value, "通过"),
-        (DesignerApplicationStatus.REFUSE.value, "拒绝")
-    ], required=True)
-    approval_opinions = serializers.CharField(max_length=512, required=False)
-    record_id = serializers.IntegerField(required=True)
-
-
-class PublishDraftSerializer(serializers.Serializer):
-
-    title = serializers.CharField(max_length=512, required=False)
-    description = serializers.CharField(max_length=512, required=False)
-    image = serializers.FileField(required=True)
-    price = serializers.FloatField(required=True)
-    category_id = serializers.IntegerField(required=True)
-
-
-class CreateOrderSerializer(serializers.Serializer):
-
-    draft_id = serializers.IntegerField(required=True)
-
-
-class OrderDeleteSerializer(serializers.Serializer):
-
-    order_id = serializers.IntegerField(required=True)
-
-
 class UserDeleteSerializer(serializers.Serializer):
 
     username = serializers.CharField(max_length=64, required=True)
